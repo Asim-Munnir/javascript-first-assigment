a24fe68dd6888200b98a1ee2a75667e810093a2d
Asim-Munnir <asimmunnir1@gmail.com>
2023-09-16 07:35:47 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,454 @@
+// ================================= chapter no 1 ==================================
+// answer 01
+
+// alert("ASALAM U ALAIKOM");
+
+
+
+
+// answer 02
+// alert("Error! Please enter a valid password")
+
+// answer 03
+// alert("Welcome to JS Land...\nHappy Coding")
+
+// answer 04
+// alert("Welcome to JS Land...")
+
+// answer 04 part 2
+// var check = document.getElementById("mycheck");
+// alert `Happy Coding!\n${check}Prevent  this page from creating additionl dialogs.`
+
+// answer 05
+// let text = prompt("Enter some message");
+// console.log(text);
+
+// answer 06
+
+
+
+// =======================================chapter 2===========================================
+// answer 01
+// var username = "";
+
+// answer 02
+// var myName = "Asim Muniir";
+
+
+
+// answer 03
+// var message = "Hello World";
+// alert(message);
+
+
+// answer 04
+// var name = "Asim Munnir";
+// var age = 18;
+// var certified = "Web Developer";
+
+// alert(name)
+// alert(age)
+// alert(certified)
+
+// answer 05
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+// answer 06
+// var email = "asimmunnir1@gmail.com";
+// alert (`My email address is ${email}`)
+
+// answer 07
+// let book = "A smarter way to learn javascript";
+// alert("i am trying to learn from the book" + book)
+
+// answer 08
+
+// var show = "Yah! I can write HTML content through Javascript"
+// document.write(show)
+
+
+// answer 09
+// let variable = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// alert(variable)
+
+
+
+
+// ================================== chapter no 03 =====================================
+// answer 01
+// var age = 18;
+// alert (`i am ${age} years old`);
+
+// answer 02
+// let time = 14
+// alert(`you have visited this site ${time} times`)
+
+
+
+// answer 03
+// var birthYear = 2005;
+// document.write(`My Birth year is ${birthYear}`);
+
+// answer 04
+// var visitor_name = "Asim"
+// var product = "T-shirts"
+// var store = "Moosajee Sons";
+// document.write(`${visitor_name} ordered 5 ${product} on ${store}`)
+
+
+// ==================================== chapter no 04 ========================================
+
+// answer 01
+// var myBikeName = "Yamaha" , myCarName = "BMW" , name = "Asim";
+ 
+// answer 02
+
+
+// answer 03
+// let a = document.querySelector(h2)
+// document.write(a)
+
+// part b
+// let b = "variable names can only contain number,$ and _ . For example $my_1stVariabl"
+// document.write(b);
+
+// part c 
+
+// let c = "Variables must begin with a lettter,$  or _ .For example $name, _name or name"
+// document.write(c);
+
+// part d
+
+// let d = "variable are case sensitive";
+// document.write(d);
+
+// part e
+
+// let e = "variables names should not be JS keywords";
+// document.write(e)
+
+
+// =============================== end of chapter 04 ==============================
+
+
+
+// ===================================chapter 05 ==================================
+
+// answer 01
+
+
+// ===================================== chapter 9 to 11 ================================
+
+
+// answer 01
+// let city = prompt("enter your city name");
+// if(city == "karachi"){
+// alert("welcome to city of lights")
+// }
+
+
+// answer 02
+// let gender = prompt("enter Gender");
+// // let male = "male"
+// // let feMale = "female"
+// if (gender == "male") {
+//     alert("Good Morning sir")
+// }
+// if(gender == "feMale"){
+//     alert("Good Morning Madam")
+// }
+
+// answer 03
+
+// let traffic = prompt("color of road traffic signal");
+// if(traffic == "red"){
+//     alert("Must Stop");
+// }
+// if (traffic == "yellow") {
+//     alert("Ready to move")
+// }
+// if (traffic == "green") {
+//     alert("Move Now")
+// }
+
+// answer 04
+// let fuel = prompt("enter remaining fuel in liters");
+// if (fuel < "0.25liters") {
+//     alert("Please refill the fuel in your car")
+// }
+
+
+// answer 05
+// part a
+// let a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// part b
+
+
+// let b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// part c
+
+// let c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+//     alert("condition 2 is true");
+// }
+// if (++c < 14){
+//     alert("condition 3 is true");
+// }
+// if(c === 14){
+//     alert("condition 4 is true");
+// }
+
+// part d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// part e
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// part f
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+// answer 06
+// let subject1 = +prompt("enter subject1 marks")  
+// let subject2 = +prompt("enter subject2 marks")  
+// let subject3 = +prompt("enter subject3 marks")
+// let marksGain = (subject1 + subject2 + subject3)
+// let percentage = marksGain * 100 /300
+// if (percentage < = 80) {
+//     alert(`marks: ${marksGain}\n grade: A-one`)
+// }  
+// if (percentage <= 70) {
+//     alert(`marks: ${marksGain}\n grade: A`)
+// }
+// if (percentage <= 60) {
+//     alert(`marks: ${marksGain}\n grade:B`)
+
+// }
+// if (percentage <= 60) {
+//     alert(`marks: ${marksGain}\n grade:Fail`)
+// }
+
+// answer 10
+// let temperature = prompt("Enter Temperature");
+// if (temperature > 40) {
+//     alert("it is too hot outside")
+// }
+// if (temperature > 30) {
+//     alert("The weather today is normal")
+// }
+// if (temperature > 20) {
+//     alert("Today's weather is cool")
+// }
+// if (temperature > 10) {
+//     alert("OMG! Today's weather is so cool")
+// }
+
+// answer 11
+
+// let firstNumber = +prompt("Enter first number");
+// let operation = prompt("enter operation")
+// let secondNumber = +prompt("Enter second number");
+
+// switch (operation) {
+//     case "+":
+//         alert(firstNumber+secondNumber)
+//         break;
+//     case "-":
+//         alert(firstNumber-secondNumber)
+//         break;
+//     case "*":
+//         alert(firstNumber*secondNumber)
+//         break;
+//     case "/":
+//         alert(firstNumber/secondNumber)
+//         break;
+//     case "%":
+//         alert(firstNumber%secondNumber)
+//         break;
+
+//     default:
+//         alert("bad Expression")
+//         break;
+// }
+
+
+// answer 07
+
+
+// answer 08
+// let number = +prompt("enter number")
+// let answer = number / 3;
+// alert(answer)
+
+// answer 09
+
+
+
+// ========================================== chapter no 12 to 13 =======================================
+
+// answer 06
+
+// var greeting;
+// var hour = 13;
+// if (hour > 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+// console.log(greeting)
+
+// answer 04
+
+// let vowel = prompt("enter vowel")
+// switch (vowel) {
+//     case "a":
+//         alert(true)
+//         break;
+//     case "e":
+//         alert(true)
+//         break;
+//     case "i":
+//         alert(true)
+//         break;
+//     case "o":
+//         alert(true)
+//         break;
+//     case "u":
+//         alert(true)
+//         break;
+
+//     default:
+//         alert(false)
+//         break;
+// }
+
+
+// answer 05
+// let correct = ("kaisa dia")
+// let password = prompt("Please enter your password");
+// if(password == correct){
+//     alert("“Correct! The password you entered matches the original password")
+// }
+// else{
+//     alert("Incorrect password");
+// }
+
+
+// answer 07
+
+// let time = +prompt("enter time");
+// if(time >= 0000 && time < 1200){
+//     alert("Good Morning");
+// }
+// else if(time >= 0000 && time < 1200){
+//     alert("Good Morning");
+// }
+// else if(time >= 1200 && time < 1700){
+//     alert("Good afternoon");
+// }
+// else if(time >= 1700 && time < 2100){
+//     alert("Good evening");
+// }
+// else{
+//     alert("Good night")
+// }
+
+
+// =================================== chapter 14 to 16 =================================
+// answer 03
+// let stringArray = ["asim" , "junaid" , "zubair"]
+
+// answer 04
+// let numberArray = [2 , 3 , 4 , 5]
+
+// answer 05
+// let booleanArray = [true , false]
+
+// answer 06
+// let mixedArray = ["asim" , 18 , false ,"web application"]
+
+// answer 07
+// let qualification = ["SSC", "HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M.Phil" , "phD"]
+// document.write(qualification)
+
+// answer 08
+// let names = ["asim" , "junaid" , "zubair"]
+// let score = [480 , 455 , 474]
+// let totalMarks = 500;
+
+// let asim = 480/totalMarks*100;
+// document.write(`Score of ${names[0]} is ${score[0]}.Percentage: ${asim}%`)
+
+// let junaid = 455/totalMarks*100;
+// document.write(`Score of ${names[1]} is ${score[1]}.Percentage: ${junaid}%`)
+
+// let zubair = 474/totalMarks*100;
+// document.write(`Score of ${names[2]} is ${score[2]}.Percentage: ${zubair}%`)
+
+// answer 10
+// let score = [320,230,480,120];
+// score.sort()
+// document.write(score)
+
+
+
+// answer 11
+// let cities = [
+//     ["karachi" , "lahore"],
+//     ["islamabad" , "quetta"],
+//     ["peshawar" , "pindi"],
+// ]
+// document.write(cities[1])
+
+
+// answer 12
+// var arr = ["This", "is", "my", "cat"];
+// var singleString = arr.join(" ");
+// console.log(singleString);
+
+
+// answer 15
+
+// var phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+
+// document.write("<select>");
+
+// for (var i = 0; i < phoneManufacturers.length; i++) {
+//   document.write("<option value='" + phoneManufacturers[i] + "'>" + phoneManufacturers[i] + "</option>");
+// }
+
+
+
+
+
+
+
+
+
+
+
