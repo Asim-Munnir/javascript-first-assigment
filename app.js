3d7b75f20b09017c904900fce0cfa4c3072ca005
Asim-Munnir <asimmunnir1@gmail.com>
2023-09-25 20:59:18 -0700

    added new work

--- app.js
+++ app.js
@@ -130,12 +130,295 @@
 
 
 // =============================== end of chapter 04 ==============================
+// Question No #1
+// var v=4,l=2,o=3
 
 
 
-// ===================================chapter 05 ==================================
+// Question No #2
+// let legalvar1="letters",legalvar2="Numbers",legalvar3="Underscore",legalvar4="$",legalvar5="CamelCase"
+// let ilegalvar1="spaces", ilegalvar2="hyphens", ilegalvar3="start with numbers", ilegalvar4="punctuation marks", ilegalvar5="JS keywords"
 
-// answer 01
+
+
+
+
+// Question No #3
+// document.write("Rules for naming JS variables <br>")
+// document.write(" Variable names can only contain letter, number,$ and underscore.For example $my_1stVariable <br>")
+// document.write("Variable names can not contain spaces, hyphens ,start with number and punctuation marks. for example 123junaid, junaid shaikh ,junaid; junaid@ <br>")
+// document.write("Variables must begin with a $, underscore or letter. For example $name, _name or name <br>")
+// document.write("Variable names are Case Sensitive <br>")
+// document.write("Variable names should not be JS keywords <br>")
+
+
+// ===================================chapter 05 ====================================
+// Question No #1
+// let sum=3
+// let sum1=5
+// let equal=sum+sum1
+// document.write(`sum of 3 and 5 is= ${equal}<br> `)
+
+
+
+
+// Question No #2
+// let sub=5
+// let sub1=3
+// let equal1=sub-sub1
+// document.write(`sub of 5 and 3 is= ${equal1} <br> `)
+// let multi=5
+// let multi1=3
+// let equal2=multi*multi1
+// document.write(`multi of 5 and 3 is= ${equal2}<br> `)
+// let divi=5
+// let divi1=3
+// let equal3=divi/divi1
+// document.write(`divi of 5 and 3 is= ${equal3}<br> `)
+// let mod=5
+// let mod1=3
+// let equal4=mod%mod1
+// document.write(`mod of 5 and 3 is= ${equal4}<br> `)
+
+
+
+
+
+
+// Question No #3
+// let a;
+// document.write(`Value after variable declaration is:${a}<br>`) 
+// a=5
+// document.write(`Intial Value:${a}<br>`)
+// ++a
+// document.write(`Value after increment is:${a}<br>`)
+// a+=7
+// document.write(`Value after addition is:${a}<br>`)
+// --a
+// document.write(`Value after Decrement is:${a}<br>`)
+// a%=3
+// document.write(`The reminder is:${a}<br>`)
+
+
+
+
+
+// Question No #4
+// let ticket=600
+// ticket*=5
+// document.write(`Total cost to buy 5 tickets to a movie is ${ticket}PKR<br>`)
+
+
+
+
+
+// Question No #5
+// document.write({"Table Of Three"})
+// let table=3
+// for(t=1; t<=10; t++){
+//     document.write(`${table}* ${t}=${table*t}<br>`) }
+
+
+
+
+
+
+
+
+
+// Question No #6
+// document.write("The Temprature Converter<br><br>")
+// let c=20
+// let far=(c*9/5)+32
+// document.write(`oC${c} is oF${far}.<br>`)
+// let f=88
+// let cal=(f-32)*5/9
+// document.write(`oF${f} is oC${cal}.<br><br>`)
+
+
+
+
+
+
+// Question No #7
+// document.write("Shoping Cart <br> <br>")
+// let item1=1200
+// let item2=1650
+// let ord1_quan=10
+// let ord2_quan=15
+// let ship_charg=500
+// document.write(`Price of 1 item is ${item1}<br>`)
+// document.write(`Quantity of 1 item is ${ord1_quan}<br>`)
+// document.write(`Price of 2 item is ${item2}<br>`)
+// document.write(`Quantity of 2 item is ${ord2_quan}<br>`)
+// document.write(`Shipping Charges is${ship_charg}<br>`)
+// let cost=item1*ord1_quan+item2*ord2_quan+ship_charg
+// document.write(`Total cost of your order is ${cost}<br>`)
+
+
+
+
+
+
+// Question No #8
+// document.write("Marks Sheet<br><br>")
+// let marks=980
+// let obt_marks=804
+// document.write(`Total Marks:${marks}<br>`)
+// document.write(`Marks Obtiend:${obt_marks}<br>`)
+// let per=(obt_marks/marks)*100
+// document.write(`Percentage:${per}%<br><br>`)
+
+
+
+
+
+
+
+// Question No #9
+// document.write("Currency Converter <br> <br>")
+// let us_dollars=300
+// let riyal=75
+// let pkr=us_dollars*10+riyal*25
+// document.write(`Total currency in PkR:${pkr}<br>`)
+
+
+
+
+
+
+// Question No #10
+// let num1=10
+// num1+=5,num1*=10,num1/=2
+// document.write(`Total is:${num1}<br><br>`)
+
+
+
+
+// Question No #11
+// document.write("Age Calculator<br>")
+// let cur_year=2023
+// document.write(`Current Year:${cur_year}<br>`)
+// let bir_year=1999
+// document.write(`Birth Year:${bir_year}<br>`)
+// let acc_age=cur_year-bir_year
+// document.write(`Your age is:${acc_age}<br><br>`)
+
+
+
+
+// Question No #12
+// document.write("The Geometrizer:<br>")
+// let radius=20
+// document.write(`Radius of circle is:${radius}<br>`)
+// let circum= (radius * 2 * Math. PI);
+// document.write(`The Circumfrence is:${circum}<br>`)
+// let area=(radius * radius * Math. PI);
+// document.write(`The area is:${area}<br><br>`)
+
+
+
+
+
+
+
+// // Question No #13
+// document.write("The Lifetime Supply Calculator:<br>" )
+// let snacks="lays"
+// document.write(`favorite snack:${snacks}<br>`)
+// let cur_age=24
+// document.write(`Current age is:${cur_age}<br>`)
+// let max_age=80
+// document.write(`Estimated Maximum age:${max_age}<br>`)
+// let amou_day=4
+// document.write(`Amount of Snacks per day:${amou_day}<br>`)
+// let rem_age=max_age-cur_age
+// rem_age*=360
+// let need=rem_age*amou_day
+// document.write(`You will need ${need} lays to last until the ripe old age of ${max_age}<br><br>`)
+
+
+
+// =================================Chapter No # 6 to 9 ===========================================
+// Question No #1
+// let x=10
+// document.write(`The value of x is:${x}<br><br>`)
+// ++x
+// document.write(`The value of ++x is:${x}<br>`)
+// document.write(`now the value of x is:${x}<br><br>`)
+// document.write(`The value of x++ is:${x}<br>`)
+// x++
+// document.write(`now the value of x is:${x}<br><br>`)
+// --x
+// document.write(`The value of --x is:${x}<br>`)
+// document.write(`now the value of x is:${x}<br><br>`)
+// document.write(`The value of x-- is:${x}<br>`)
+// x--
+// document.write(`now the value of x--is:${x}<br><br>`)
+
+
+
+
+
+
+// Question No #2
+// let y=3,z=4
+// document.write(`Y is:${y}<br>Z is:${z}<br>`)
+// --y;
+// document.write(`Y is:${y}<br>Z is:${z}<br>`)
+// --y - --z;
+// document.write(`Y is:${y}<br>Z is:${z}<br>`)
+// --y - --z + ++z;
+// document.write(`Y is:${y}<br>Z is:${z}<br>`)
+// let resultes=--y - --z + ++z + z--;
+// document.write(`Result is:${resultes}<br>`)
+
+
+
+
+// Question No #3
+// prompt("Asslamu Alikum \n Enter Your Name")
+
+
+// Question No #4
+//  Sir Quetion No 4 in asigment is Empty
+
+
+
+
+// Question No #5
+// let number=+prompt("Enter any number do you want a table ",5)
+// for(i=1; i<=10; i++){
+//     document.write(`${number}* ${i}=${number*i}<br>`) }
+
+
+
+
+
+
+//Question No #5
+// let subj1=prompt("Enter your first subject")
+// let subj2=prompt("Enter your Second subject")
+// let subj3=prompt("Enter your Third subject")
+// let totmark=100
+// let subj1mark=+prompt("Enter your first subject Obtained Marks")
+// let subj2mark=+prompt("Enter your Second subject Obtained Marks")
+// let subj3mark=+prompt("Enter your Third subject Obtained Marks")
+// let percentage1=(subj1mark*100)/totmark
+// let percentage2=(subj2mark*100)/totmark
+// let percentage3=(subj3mark*100)/totmark
+// document.write(`Subject TotalMarks Obtained Marks Percentage<br>`)
+// document.write(`${subj1}---------${totmark}---------${subj1mark}----------${percentage1}% <br>`)
+// document.write(`${subj2}---------${totmark}---------${subj2mark}----------${percentage2}% <br>`)
+// document.write(`${subj3}---------${totmark}---------${subj3mark}----------${percentage3}% <br>`)
+// totmark*=3
+// let totobt=subj1mark+subj2mark+subj3mark
+// let totper=(percentage1+percentage2+percentage3)/3
+// document.write(`---------${totmark}---------${totobt}----------${totper}% <br><br>`)
+
+
+
+
 
 
 // ===================================== chapter 9 to 11 ================================
